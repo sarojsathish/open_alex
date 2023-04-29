0237bdc0995eceb08c64ffde4a4ee3f0b0f83c44
sarojsathish <112919815+sarojsathish@users.noreply.github.com>
2023-04-28 23:14:39 -0400

    setup.py file uploaded

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Setup script for pkg package.
+"""
+
+from setuptools import setup
+
+setup(name='openalex',
+      version='0.0.1',
+      
+      description='bibtex and ris utilities',
+      maintainer='saaksshi',
+      maintainer_email='sjilhewa@andrew.cmu.edu',
+      license='MIT',
+      packages=['openalex'],
+      scripts=[],
+      entry_points={
+        "console_scripts": ["cite = openalex.cmdline:cite"]},
+      long_description='''to get citations bibtex/ris''')
