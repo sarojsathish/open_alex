af9a4f9bf035b7c76c1bc3889a55c402b5329958
sarojsathish <112919815+sarojsathish@users.noreply.github.com>
2023-04-28 23:32:57 -0400

    Update test.py

--- openalex/test.py
+++ openalex/test.py
@@ -4,9 +4,9 @@ Tests to ensure pacakge is working.
 from .works import Works
 TITLE = "Examples of Effective Data Sharing in Scientific Publishing"
 
-def test_bibtex():
+def test_bib():
     """
     Tests to check working function in package.
     """
-    won = Works("https://doi.org/10.1021/acscatal.5b00538")
-    assert TITLE == won.new()
+    req = Works("https://doi.org/10.1021/acscatal.5b00538")
+    assert TITLE == req.new()
