a2473dc7a02babe76a7fa9c8a105e17376c36c34
sarojsathish <112919815+sarojsathish@users.noreply.github.com>
2023-04-28 23:33:50 -0400

    Update cmdline.py

--- openalex/cmdline.py
+++ openalex/cmdline.py
@@ -21,8 +21,8 @@ def cite(doi, citeformat):
     Outputs RIS or BibTeX for a given DOI. ChatGPT 4 assisted
     me in writing this function.
     """
-    w_0 = Works(doi)
+    abc = Works(doi)
     if citeformat == "bibtex":
-        print(w_0.bibtex())
+        print(abc.bibtex())
     elif citeformat == "ris":
-        print(w_0.ris())
+        print(abc.ris())
